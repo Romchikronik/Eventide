a16e2bf77f981cd2e557de40cc0faf96a07c87e4
Romchik <roma.kolesov.00.00@mail.ru>
2021-03-13 20:39:41 +0500

    исправил

--- js/script.js
+++ js/script.js
@@ -23,38 +23,4 @@ window.addEventListener('load', function(){
         }
        
     });
-    
-    var register = document.querySelector('.register');
-    var discover = document.querySelector('.discover');
-    var cross = document.querySelectorAll('.cross');
-    var modal1 = document.querySelector('.modal1');
-    var modal2 = document.querySelector('.modal2');
-    var over = document.querySelector('.overlay');
-    
-    var body = document.querySelector('body')
-    body.addEventListener('click', function(e){
-        if(e.target == over){
-            modal1.classList.remove('modal-open1');
-            modal2.classList.remove('modal-open2');
-            over.style.display = 'none';
-        }
-    })
-    
-    register.addEventListener('click', function(event){
-        event.preventDefault();
-        over.style.display = 'block';
-        modal1.classList.add('modal-open1');
-    })
-    discover.addEventListener('click', function(event){
-        event.preventDefault();
-        over.style.display = 'block';
-        modal2.classList.add('modal-open2');
-    })
-    for(var i = 0; i < cross.length; i++){
-        cross[i].addEventListener('click', function(){
-            modal1.classList.remove('modal-open1');
-            modal2.classList.remove('modal-open2');
-            over.style.display = 'none';
-        })
-    }
-})
+ })
